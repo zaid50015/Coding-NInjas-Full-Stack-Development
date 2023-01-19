3483e35cae97e59b36c112dadf5fea3983cbea82
Mohammad Zaid <mohmmadzaid759@gmail.com>
2023-01-19 21:40:51 +0530

    calculator project

---
+++ Calculator-Project/expermient.js
@@ -0,0 +1,14 @@
+let a="";
+a=a.concat("1");
+a=a.concat("+");
+a=a.concat("2");
+console.log(a);
+console.log(eval(a));
+
+let b="";
+b+="1";
+b+="3";
+b+="+";
+b+="2";
+console.log(b);
+console.log(eval(b));
